471a5a2bafd37c92fb523ec37228de9946d1e5c3
Santos307 <candidocavaleiro@outlook.com>
2021-04-07 23:21:44 -0300

    criando componente entrar e cadastrar

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditarComponent } from './editar/postagem-editar/postagem-editar.component';
+import { TemaEditarComponent } from './editar/tema-editar/tema-editar.component';
+import { UserEditarComponent } from './editar/user-editar/user-editar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
-//O path é o nome do componente.
+//O path é o nome do componente da minha rota.
 const routes: Routes = [
   { path: '', redirectTo: 'entrar', pathMatch: 'full' },
 
   { path: 'entrar', component: EntrarComponent },
   { path: 'cadastrar', component: CadastrarComponent },
+
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent},
+
+  {path: 'tema-editar/:id', component: TemaEditarComponent},
+  {path: 'tema-delete/:id', component: TemaDeleteComponent},
+  {path: 'postagem-editar/:id', component: PostagemEditarComponent},
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent},
+  {path: 'user-editar/:id', component: UserEditarComponent}
 ];
 
 @NgModule({
@@ -16,5 +32,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
-
-
